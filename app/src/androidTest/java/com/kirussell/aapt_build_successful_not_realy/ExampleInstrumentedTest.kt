74c6f325f81941a00390e1b821605e6fdee43927
kirussell <ruslan.kim@grabtaxi.com>
2017-09-04 17:24:19 +0800

    initial commit

---
+++ app/src/androidTest/java/com/kirussell/aapt_build_successful_not_realy/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.kirussell.aapt_build_successful_not_realy
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.kirussell.aapt_build_successful_not_realy", appContext.packageName)
+    }
+}
